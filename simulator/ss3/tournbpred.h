533791e25b304ee37fc1501f312f51ac2c80afe7
Erik <erik@EJM002.(none)>
2015-05-05 17:42:51 -0700

    adding branch predictor struct header file

---
+++ simulator/ss3/tournbpred.h
@@ -0,0 +1,24 @@
+#ifndef TOURNBPRED_H
+#define TOURNBPRED_H
+
+#define dassert(a) assert(a)
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define LHistMask 0x0003FF
+#define LPredMask 0x000003FF
+#define GPredMask 0x00000FFF
+#define CPredMask 0x00000FFF
+#define PHistMask 0x00000FFF
+
+struct alpha_Bpred {
+    int LocalHist[1024];    // size of PC = bits
+    int LocalPred[1024];    // 10 bits
+    int GlobalPred[4096];   // 12 bits
+    int ChoicePred[4096];   // 12 bits
+    int PathHist;           // 12 bits
+    bool BPred;             
+};
+
+#endif /* BPRED_H */
