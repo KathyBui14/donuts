0cfca0249331333bd00229546598ff4e274d5bee
Jordan Fluth <jfluth@gmail.com>
2015-05-07 22:43:14 -0700

    added some functions... the project builds.

--- simulator/ss3/tournbpred.h
+++ simulator/ss3/tournbpred.h
@@ -13,12 +13,14 @@
 #define PHistMask 0x00000FFF
 
 struct alpha_Bpred {
-    int LocalHist[1024];    // size of PC = bits, NEED TO CHECK!
-    int LocalPred[1024];    // 10 bits
-    int GlobalPred[4096];   // 12 bits
-    int ChoicePred[4096];   // 12 bits
-    int PathHist;           // 12 bits
-    bool BPred;             
-};
+  int LocalHist[1024];    // size of PC = bits, NEED TO CHECK!
+  int LocalPred[1024];    // 10 bits
+  int GlobalPred[4096];   // 12 bits
+  int ChoicePred[4096];   // 12 bits
+  unsigned int PathHist;  // 12 bits
+	int LHistory;
+	bool is_global;
+  bool BPred;             
+}alpha;
 
 #endif /* BPRED_H */
